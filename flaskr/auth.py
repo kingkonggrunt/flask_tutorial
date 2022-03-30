8d326238623bae0582c1ace0c46558b28490056e
kingkonggrunt <little-kk@hotmail.com>
2022-03-30 12:32:12 +1100

    feat: added user authentication

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -2,39 +2,44 @@ from crypt import methods
 import functools
 
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
-from werkzeug.security import check_password_hash, generate_password_hash, 
+from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route("/register", methods=('GET','POST'))
 def register():
+    """Create a new user account"""
     if request.method == 'POST':
-        """Create a new user account"""
         username = request.form['username']
         password = request.form['password']
         db = get_db()
+        error = None
         
         if not username:
-            flash("Username is required")
+            error = "Username is required"
         elif not password:
-            flash("Password is required")
-            
-        try:
-            db.execute(
-                "INSERT INTO user (username, password) VALUES (?, ?)",
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-        except db.IntegrityError:
-            flash(f"User {username} already registered")
-        else:
-            return redirect(url_for("auth.login"))
+            error = "Password is required"
+        
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} already registered"
+            else:
+                return redirect(url_for("auth.login"))
+    
+        flash(error)
         
     return render_template("auth/register.html")
 
 @bp.route("/login", methods=('GET', 'POST'))
 def login():
+    """Login an existing user"""
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
@@ -60,6 +65,7 @@ def login():
 
 @bp.before_app_request
 def load_logged_in_user():
+    """Check that the user is logged in"""
     user_id = session.get('user_id')
     
     if user_id is None:
@@ -71,10 +77,12 @@ def load_logged_in_user():
         
 @bp.route("/logout")
 def logout():
+    """Log out the user"""
     session.clear()
     return redirect(url_for('index'))
 
 def login_required(view):
+    """Wraps a view so that it requires authentication to access"""
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
